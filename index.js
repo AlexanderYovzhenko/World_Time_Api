2f93f4435b3af01014401b8af8b096c6d69946df
Alexander <renyreny125tt@gmail.com>
2022-08-06 15:06:15 +0300

    refactor: update function queryTimeApi

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const body = document.querySelector('body');
 const site = 'http://worldtimeapi.org/api/timezone/Asia/Ho_Chi_Minh';
 const interval = 1000;
 
-async function queryTimeApi (site) {
-  return await (await fetch(site)).text();
+function queryTimeApi (site) {
+  setInterval(async () => {
+    try {
+      const response = await fetch(site);
+      const data = await response.text();
+      body.innerText = data;
+    } catch (error) {
+      body.innerText = error;
+      console.error(error);
+    }
+  }, interval);
 };
 
-setInterval(() => {
-  queryTimeApi(site)
-    .then(data => {
-      body.innerText = data;
-    })
-    .catch(err => {
-      body.innerText = err;
-      console.error(err);
-    });
-}, interval);
+queryTimeApi(site);
