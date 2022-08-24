063605f03ac463888852efe802cf3c0031ce8403
Alexander <renyreny125tt@gmail.com>
2022-08-24 14:12:38 +0300

    feat: update request API every 1s

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
-import http from 'http';
-
-const host = 'worldtimeapi.org';
-const path = '/api/timezone/Asia/Ho_Chi_Minh';
+const url = 'http://worldtimeapi.org/api/timezone/Asia/Ho_Chi_Minh';
 const interval = 1000;
 
-const options = {
-  method: 'GET',
-  host: host,
-  path: path
+
+const awaitTimeout = (interval) => {
+  return new Promise(resolve => setTimeout(resolve, interval));
 };
 
-const callback = (response) => {
-  let answer = '';
-  response.on('data', (chunk) => {
-    answer += chunk.toString();
-  });
+const getWorldTimeApi = async () => {
+  const res = await fetch('http://worldtimeapi.org/api/timezone/Asia/Ho_Chi_Minh');
 
-  response.on('end', () => {
-    console.log(answer, '\n__________________\n');
-  });
+  if (res.ok) {
+      const data = await res.text();
+      console.log(data, '\n');
+      const timeout = await awaitTimeout(interval);
+      getWorldTimeApi();
+      clearTimeout(timeout);
+  }
 };
 
-setInterval(() => {
-  const request = http.request(options, callback).end();
-  request.on('error', (err) => {
-    console.error(err);
-  });
-}, interval);
+getWorldTimeApi();
+
+// import { request } from 'https';
+
+// const host = 'worldtimeapi.org';
+// const path = '/api/timezone/Asia/Ho_Chi_Minh';
+
+// const options = {
+//   timeout: 5000,
+//   host: host,
+//   path: path
+// };
+
+// const callback = (response) => {
+//   let answer = '';
+//   response.on('data', (chunk) => {
+//     answer += chunk.toString();
+//   });
+
+//   response.on('end', () => {
+//     console.log(answer, '\n__________________\n');
+//   });
+// };
+
+
+// function doRequest(options) {
+//   return new Promise ( async (resolve, reject) => {
+//     let req = request(options, callback);
+
+//     req.on('response', res => {
+//       resolve(res);
+//     });
+
+//     req.on('error', err => {
+//       reject(err);
+//     });
+
+//     req.end();
+
+//   }); 
+// }
+
+
+// const worldTimeApiRequest = async () => {
+//   try {
+//     await doRequest(options);
+//   } catch (error) {
+//     console.log(error, '111111111');
+//   } 
+//   const timer = await awaitTimeout(interval);
+//   worldTimeApiRequest(); 
+//   clearTimeout(timer);
+// }
+
+// worldTimeApiRequest();
